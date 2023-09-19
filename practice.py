00184864b4c6e21fe883f497b605bf56c76fe4a7
Bonzo <zylexbonzo64@gmail.com>
2023-09-19 20:38:44 +0800

    Modification and New action
    1. Update concatenation
    2. User Input test

--- practice.py
+++ practice.py
@@ -1,4 +1,11 @@
+# # Concatenate 2 int variables
 x = 1
 y = 2 
+z = x + y
 
-print(x+y)
+print(z)
+
+
+# Get a user input and return said user input
+name = str(input("What is your name? \n"))
+print("Hello " + name + "!")
